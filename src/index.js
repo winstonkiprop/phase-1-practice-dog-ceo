c4c5fa8441b9a7b50050edb457b950f949d0115b
winston kiprop <winstonekip50@gmail.com>
2022-06-20 10:06:38 +0300

    commit tree 6613e1c3ef94a4efe4fddf9c5818655323684ac2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log('%c HI', 'color: firebrick')
+
+console.log("% HI", "color: fireback");
+document.addEventListener("DOMContentLoaded", function () {
+    fetchDogImages();
+    breeds();
+});
+function fetchDogImages() {
+  fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then((response) => response.json())
+    .then((data) => {
+      //console.log(data);
+      let images = data.message;
+      //   console.log(images)
+      images.forEach((image) => {
+        //  console.log(image)
+        loadImagesToDom(image);
+      });
+    });
+}
+function loadImagesToDom(image,) {
+  let dogCount = document.getElementById("dog-image-container");
+  let ourImage = document.createElement("img");
+  ourImage.src = image;
+  dogCount.appendChild(ourImage);
+}
+function breeds ()
+{
+    fetch( "https://dog.ceo/api/breeds/list/all" )
+        .then( ( response ) => response.json() )
+        .then( ( dogs ) =>
+        {
+        console.log(dogs);
+    })
+}
